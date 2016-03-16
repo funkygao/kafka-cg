494d76fd522ff30f4d7682eff2ca71677e1c815f
funkygao <funky.gao@gmail.com>
2016-03-16 14:17:32 +0800

    reformat

--- consumergroup/config.go
+++ consumergroup/config.go
@@ -14,10 +14,20 @@ type Config struct {
 	Zookeeper *kazoo.Config
 
 	Offsets struct {
-		Initial           int64         // The initial offset method to use if the consumer has no previously stored offset. Must be either sarama.OffsetOldest (default) or sarama.OffsetNewest.
-		ProcessingTimeout time.Duration // Time to wait for all the offsets for a partition to be processed after stopping to consume from it. Defaults to 1 minute.
-		CommitInterval    time.Duration // The interval between which the processed offsets are commited.
-		ResetOffsets      bool          // Resets the offsets for the consumergroup so that it won't resume from where it left off previously.
+		// The initial offset to use if the consumer has no previously stored offset.
+		// Must be either sarama.OffsetOldest (default) or sarama.OffsetNewest.
+		Initial int64
+
+		// Resets the offsets for the consumer group so that it won't resume
+		// from where it left off previously.
+		ResetOffsets bool
+
+		// Time to wait for all the offsets for a partition to be processed
+		// after stopping to consume from it.
+		ProcessingTimeout time.Duration
+
+		// The interval between which the processed offsets are commited.
+		CommitInterval time.Duration
 	}
 }
 
