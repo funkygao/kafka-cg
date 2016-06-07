efa7bfe9feac56ea096ae548baba1da163f30410
funkygao <funky.gao@gmail.com>
2016-06-07 21:30:04 +0800

    kill the unused line of code
    consumer id is generated in pkg: kazoo-go

--- consumergroup/utils.go
+++ consumergroup/utils.go
@@ -1,10 +1,6 @@
 package consumergroup
 
 import (
-	"crypto/rand"
-	"fmt"
-	"io"
-	"os"
 	"sort"
 
 	"github.com/funkygao/kazoo-go"
@@ -59,34 +55,4 @@ func dividePartitionsBetweenConsumers(consumers kazoo.ConsumergroupInstanceList,
 	}
 
 	return result
-}
-
-func generateUUID() (string, error) {
-	uuid := make([]byte, 16)
-	n, err := io.ReadFull(rand.Reader, uuid)
-	if n != len(uuid) || err != nil {
-		return "", err
-	}
-	// variant bits; see section 4.1.1
-	uuid[8] = uuid[8]&^0xc0 | 0x80
-	// version 4 (pseudo-random); see section 4.1.3
-	uuid[6] = uuid[6]&^0xf0 | 0x40
-	return fmt.Sprintf("%x-%x-%x-%x-%x", uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:]), nil
-}
-
-func generateConsumerID() (consumerID string, err error) {
-	var uuid, hostname string
-
-	uuid, err = generateUUID()
-	if err != nil {
-		return
-	}
-
-	hostname, err = os.Hostname()
-	if err != nil {
-		return
-	}
-
-	consumerID = fmt.Sprintf("%s:%s", hostname, uuid)
-	return
 }
